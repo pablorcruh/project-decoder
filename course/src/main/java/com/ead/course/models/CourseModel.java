fbb0ec29582319baafac905c6861ee4d3ca8a023
pablorcruh <pablorcruh@gmail.com>
2022-02-13 18:52:53 -0500

    Add relationship between lesson, module and course

--- course/src/main/java/com/ead/course/models/CourseModel.java
+++ course/src/main/java/com/ead/course/models/CourseModel.java
@@ -4,17 +4,19 @@ import com.ead.course.enums.CourseLevel;
 import com.ead.course.enums.CourseStatus;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 
 import javax.persistence.*;
 import java.io.Serializable;
 import java.time.LocalDateTime;
+import java.util.Set;
 import java.util.UUID;
 
 @Data
 @JsonInclude(JsonInclude.Include.NON_NULL)
 @Entity
-@Table(name = "tb_courses")
+@Table(name = "TB_COURSES")
 public class CourseModel implements Serializable {
 
     private static final long serialVersionUID = 1L;
@@ -50,5 +52,9 @@ public class CourseModel implements Serializable {
 
     @Column(nullable = false)
     private UUID userInstructor;
+
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    @OneToMany(mappedBy = "course")
+    private Set<ModuleModel> modules;
 
 }
--- course/src/main/java/com/ead/course/repositories/LessonRepository.java
+++ course/src/main/java/com/ead/course/repositories/LessonRepository.java
@@ -1,8 +1,11 @@
 package com.ead.course.repositories;
 
+import com.ead.course.models.LessonModel;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
-public interface LessonRepository extends JpaRepository<LessonRepository, UUID> {
+@Repository
+public interface LessonRepository extends JpaRepository<LessonModel, UUID> {
 }
