55fbfc97c7f707c57b99fc0d17e0073c509772e5
pablorcruh <pablorcruh@gmail.com>
2022-03-05 18:15:31 -0500

    Composition pattern courses microservice

---
+++ course/src/main/java/com/ead/course/services/impl/CourseUserServiceImpl.java
@@ -0,0 +1,8 @@
+package com.ead.course.services.impl;
+
+import com.ead.course.services.CourseUserService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CourseUserServiceImpl implements CourseUserService {
+}
