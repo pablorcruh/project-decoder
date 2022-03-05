4bf93895bfa1861cbf603b9ba280ba1542dbea7a
pablorcruh <pablorcruh@gmail.com>
2022-03-04 22:03:11 -0500

    Agregar ISO 8601 UTC

--- authuser/src/main/java/com/ead/authuser/models/UserModel.java
+++ authuser/src/main/java/com/ead/authuser/models/UserModel.java
@@ -56,10 +56,10 @@ public class UserModel extends RepresentationModel<UserModel> implements Seriali
     private String imageUrl;
 
     @Column(nullable = false)
-    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy HH:mm:ss")
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'")
     private LocalDateTime creationDate;
 
-    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy HH:mm:ss")
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'")
     @Column(nullable = false)
     private LocalDateTime lastUpdateDate;
 
