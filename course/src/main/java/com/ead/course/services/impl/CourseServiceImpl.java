c720a7cf6ffa546e70b54bf064de94e57411cc59
pablorcruh <pablorcruh@gmail.com>
2022-06-09 19:58:35 -0500

    Create publisher command message

--- course/src/main/java/com/ead/course/services/impl/CourseServiceImpl.java
+++ course/src/main/java/com/ead/course/services/impl/CourseServiceImpl.java
@@ -1,13 +1,17 @@
 package com.ead.course.services.impl;
 
+import com.ead.course.dtos.NotificationCommandDto;
 import com.ead.course.models.CourseModel;
 import com.ead.course.models.LessonModel;
 import com.ead.course.models.ModuleModel;
+import com.ead.course.models.UserModel;
+import com.ead.course.publishers.NotificationCommandPublisher;
 import com.ead.course.repositories.CourseRepository;
 import com.ead.course.repositories.LessonRepository;
 import com.ead.course.repositories.ModuleRepository;
 import com.ead.course.repositories.UserRepository;
 import com.ead.course.services.CourseService;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -20,6 +24,7 @@ import java.util.Optional;
 import java.util.UUID;
 
 @Service
+@Log4j2
 public class CourseServiceImpl implements CourseService {
 
     @Autowired
@@ -33,6 +38,9 @@ public class CourseServiceImpl implements CourseService {
 
     @Autowired
     UserRepository courseUserRepository;
+
+    @Autowired
+    NotificationCommandPublisher notificationCommandPublisher;
 
     @Transactional
     @Override
@@ -75,5 +83,20 @@ public class CourseServiceImpl implements CourseService {
     @Override
     public void saveSubscriptionUserInCourse(UUID courseId, UUID userId) {
         courseRepository.saveCourseUser(courseId, userId);
+    }
+
+    @Transactional
+    @Override
+    public void saveSubscriptionUserInCourseAndSendNotification(CourseModel course, UserModel user){
+        courseRepository.saveCourseUser(course.getCourseId(), user.getUserId());
+        try{
+            var notificationCommandDto = new NotificationCommandDto();
+            notificationCommandDto.setTitle("Bienvenido al Curso: " + course.getName());
+            notificationCommandDto.setMessage(user.getFullName() + "su inscripci'on se ha realizado con exito");
+            notificationCommandDto.setUserId(user.getUserId());
+            notificationCommandPublisher.publishNotificationCommand(notificationCommandDto);
+        }catch(Exception e){
+            log.error(e.getMessage());
+        }
     }
 }
