b5227b0202fb5cda24ed9b26a52a840be8607bb8
pablorcruh <pablorcruh@gmail.com>
2022-06-18 21:51:20 -0500

    Basic authentication in AuthUser microservice using roles

---
+++ authuser/src/main/resources/sql/roles.sql
@@ -0,0 +1,4 @@
+insert into tb_roles values ('ced0f241-a468-4e55-a912-6792dd2590c1', 'ROLE_ADMIN');
+insert into tb_roles values ('4c249835-b760-4686-b0da-2b79be90e9bd', 'ROLE_INSTRUCTOR');
+insert into tb_roles values ('6ce7e051-7ad1-41c6-8593-6fcc9926d8cf', 'ROLE_STUDENT');
+insert into tb_roles values ('f2531270-680c-40d7-9a1f-d213cc4c2ba3', 'ROLE_USER');
